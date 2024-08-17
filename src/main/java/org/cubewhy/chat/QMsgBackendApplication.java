b5f98ed1e62ae2cf546f95220304d5d7598fca02
cubewhy <cubewhy@163.com>
2024-08-17 10:12:51 +0800

    feat: enable cache

--- src/main/java/org/cubewhy/chat/QMsgBackendApplication.java
+++ src/main/java/org/cubewhy/chat/QMsgBackendApplication.java
@@ -2,8 +2,10 @@ package org.cubewhy.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class QMsgBackendApplication {
 
     public static void main(String[] args) {
