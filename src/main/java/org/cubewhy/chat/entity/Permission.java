913745cce29a88811af7da7f0f258a3dbce3a526
cubewhy <cubewhy@163.com>
2024-08-19 07:37:27 +0800

    feat: manage files

--- src/main/java/org/cubewhy/chat/entity/Permission.java
+++ src/main/java/org/cubewhy/chat/entity/Permission.java
@@ -5,6 +5,7 @@ public enum Permission {
     DASHBOARD, // 访问后端的仪表盘
     MANAGE_USER, // 管理所有用户
     MANAGE_ROLES, // 管理身份组
+    MANAGE_FILES, // 管理用户上传的文件
     // servlet admin & channel admin rights
     MANAGE_CHANNEL, // 管理频道
     REGISTER_INVITE, // 生成注册邀请
@@ -19,5 +20,5 @@ public enum Permission {
     JOIN_CHANNEL, // 加入频道
     VIEW_CHANNEL,
     SEND_CHANNEL_INVITE, // 发送加频道邀请
-    UPLOAD_FILES
+    UPLOAD_FILES // 上传文件
 }
