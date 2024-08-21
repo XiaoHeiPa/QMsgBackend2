255657cffd8436926d03be18849735f8f1658de8
cubewhy <cubewhy@163.com>
2024-08-21 13:40:05 +0800

    fcm config

--- src/main/java/org/cubewhy/chat/config/PushConfig.java
+++ src/main/java/org/cubewhy/chat/config/PushConfig.java
@@ -4,35 +4,34 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 @Configuration
 public class PushConfig {
-//    @Bean
-//    FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
-//        return FirebaseMessaging.getInstance(firebaseApp);
-//    }
-
-//    @Bean
-//    FirebaseApp firebaseApp(GoogleCredentials credentials) {
-//        FirebaseOptions options = FirebaseOptions.builder()
-//                .setCredentials(credentials)
-//                .build();
-//
-//        return FirebaseApp.initializeApp(options);
-//    }
+    @Value("${spring.application.push.fcm.config}")
+    String fcmConfigPath;
 
     @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+    FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+    @Bean
+    FirebaseApp firebaseApp(GoogleCredentials credentials) {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging(GoogleCredentials googleCredentials) throws IOException {
         FirebaseOptions firebaseOptions = FirebaseOptions
                 .builder()
                 .setCredentials(googleCredentials)
@@ -41,16 +40,10 @@ public class PushConfig {
         return FirebaseMessaging.getInstance(app);
     }
 
-//    @Bean
-//    GoogleCredentials googleCredentials() throws IOException {
-//        if (firebaseProperties.getServiceAccount() != null) {
-//            try (InputStream is = firebaseProperties.getServiceAccount().getInputStream()) {
-//                return GoogleCredentials.fromStream(is);
-//            }
-//        } else {
-//            // Use standard credentials chain. Useful when running inside GKE
-//            return GoogleCredentials.getApplicationDefault();
-//        }
-//    }
+    @Bean
+    GoogleCredentials googleCredentials() throws IOException {
+        return GoogleCredentials
+                .fromStream(new FileInputStream(fcmConfigPath.replaceFirst("~", System.getProperty("user.home"))));
+    }
 
 }
